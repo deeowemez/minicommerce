3bd59f7ab1af6275de8d3b71ea137232d5a27f63
Marianne Domilies <53809101+deeowemez@users.noreply.github.com>
2025-08-28 22:56:24 +0800

    modify dynamo client script

--- backend/db/dynamoClient.js
+++ backend/db/dynamoClient.js
@@ -6,8 +6,8 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({
-  region: import.meta.env.AWS_REGION,
-  endpoint: import.meta.env.DYNAMODB_ENDPOINT,
+  region: process.env.AWS_REGION,
+  endpoint: process.env.DYNAMODB_ENDPOINT,
   credentials: {
     accessKeyId: "dummy",
     secretAccessKey: "dummy",
